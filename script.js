5d292e3ef832179a27444c9c44b1d6a70ef0c736
Carter-Y <129871503+Carter-Y@users.noreply.github.com>
2023-04-16 13:05:31 -0400

    fixed the link not being clickable

--- script.js
+++ script.js
@@ -1,27 +1,31 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const tabs = document.querySelectorAll(".tab");
-    const navLinks = document.querySelectorAll("nav a");
-  
-    function showTab(id) {
-      tabs.forEach(tab => {
-        if (tab.id === id) {
-          tab.style.display = "block";
-        } else {
-          tab.style.display = "none";
-        }
-      });
-    }
-  
-    function handleNavLinkClick(event) {
-      event.preventDefault();
-      const id = event.target.getAttribute("href").substr(1);
-      showTab(id);
-    }
-  
-    navLinks.forEach(link => {
-      link.addEventListener("click", handleNavLinkClick);
+document.addEventListener("DOMContentLoaded", function () {
+  const tabs = document.querySelectorAll(".tab");
+  const navLinks = document.querySelectorAll("nav a");
+
+  function showTab(id) {
+    tabs.forEach(tab => {
+      if (tab.id === id) {
+        tab.style.display = "block";
+      } else {
+        tab.style.display = "none";
+      }
     });
-  
-    showTab("tab1");
+  }
+
+  function handleNavLinkClick(event) {
+    event.preventDefault();
+    const id = event.target.getAttribute("href").substr(1);
+    showTab(id);
+  }
+
+  navLinks.forEach(link => {
+    link.addEventListener("click", handleNavLinkClick);
   });
-  
+
+  showTab("tab1");
+});
+
+const rect = document.querySelector(".rect");
+rect.addEventListener("click", function() {
+  window.location.href = "https://drive.google.com/drive/folders/1_yyIMneYBa8vWJuyU8DUaHSBdddfK4Jy?usp=sharing";
+});
